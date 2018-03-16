3304696c79a6d1461aa043baa3acbbe167163d56
Dmitri Pisarev <dimaip@gmail.com>
2018-03-16 14:44:57 +0300

    TASK: update Readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Guided Tour
+
+## Development
+
+Go to `Resources/Private/JavaScript/Hint` and run `yarn`. Then run `yarn build` or `yarn watch` to compile the assets.
+
+So far all the enjoyhint initialization code is in the middle of `manifest.js` file. Feel free to refactor it as pleased.
--- Resources/Private/JavaScript/Hint/src/manifest.js
+++ Resources/Private/JavaScript/Hint/src/manifest.js
@@ -4,22 +4,14 @@ import {actionTypes} from '@neos-project/neos-ui-redux-store';
 
 export function* watchReady() {
     yield takeLatest(actionTypes.System.READY, function* ready(action) {
-        //initialize instance
-        var enjoyhint_instance = new EnjoyHint({});
-
-        //simple config. 
-        //Only one step - highlighting(with description) "New" button 
-        //hide EnjoyHint after a click on the button.
-        var enjoyhint_script_steps = [
+        const enjoyhint_instance = new EnjoyHint({});
+        // See the docs: https://github.com/xbsoftware/enjoyhint/#initialization-and-configuration
+        const enjoyhint_script_steps = [
             {
-                'click #neos-PageTree-AddNode': 'Click the "New" button to start creating your project'
+                'click #neos-PageTree-AddNode': 'Click the "Plus" button to create a new document node'
             }
         ];
-
-        //set script config
         enjoyhint_instance.set(enjoyhint_script_steps);
-
-        //run Enjoyhint script
         enjoyhint_instance.run();
     });
 }
